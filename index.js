ddd7795819890161493a88a288a1070218c3f0d9
Tommy <tooto1985@gmail.com>
2020-08-23 20:03:09 +0800

    modify

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+
+
+
 const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql `
+  type book {
+    id: Int,
+    name: String
+  }
+
   type Query {
     "A simple type for getting started!"
     hello: String,
     id: Int
+    books: [book]
+    book(name: String!): book
+    bookById(id: Int!): book
   }
 `;
 const resolvers = {
   Query: {
     hello: () => 'world',
-    id: () => 5520
+    id: () => 5520,
+    books: () => db.books,
+    book:  (root, { name }, context) => {
+      return db.books.find(e=>e.name === name);
+    },
+    bookById: (root, { id }, context) => {
+      return db.books.find(e=>e.id === id);
+    },
   }
 };
 const server = new ApolloServer({
@@ -19,3 +37,19 @@ const server = new ApolloServer({
 server.listen().then(({url}) => {
   console.log(`? server ready at ${url}`);
 });
+// ==== data ====
+const db = {};
+db.books = [
+  {
+    id: 1,
+    name: 'T1'
+  },
+  {
+    id: 2,
+    name: 'T2'
+  },
+  {
+    id: 3,
+    name: 'T3'
+  }
+];
